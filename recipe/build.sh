eeff047d975dd09a7da2b52422edf18008ef1d35
Jan Janssen <jan-janssen@users.noreply.github.com>
2024-02-15 18:53:09 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,7 +5,7 @@ cd src-devel
 
 # export flags
 export FFLAGS="${FFLAGS} -fallow-argument-mismatch"
-export LDFLAGS="${LDFLAGS} -llapack -lblas -lcblas -fopenmp"
+export LIB="${LDFLAGS} -llapack -lblas -fopenmp"
 
 # serial version
 make FC=${FC} DEBUG=OFF MKL=OFF PROGRAM=RuNNer.serial.x
