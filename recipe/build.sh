d1a70ff174cb1d3bda0761c47eeb6fe144542fb7
Jan Janssen <jan-janssen@users.noreply.github.com>
2024-02-15 14:21:30 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
+# create working directory
 mkdir -p ${PREFIX}/bin
 cd src-devel
 
-make -f Makefile serial
-cp RuNNer.serial.x ${PREFIX}/bin
-make -f Makefile clean
+# remove other make files
+rm GNUmakefile
+rm Makefile.orig
 
-make -f Makefile mpi
+# serial version
+make serial
+cp RuNNer.serial.x ${PREFIX}/bin
+make clean
+
+# mpi parallel version
+make mpi
 cp RuNNer.mpi.x ${PREFIX}/bin
