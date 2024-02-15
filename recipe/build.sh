8a815a1afc4cb03398646b35e43842fda051bd92
Jan Janssen <jan-janssen@users.noreply.github.com>
2024-02-15 15:29:42 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,10 +7,10 @@ cd src-devel
 export FFLAGS="${FFLAGS} -fallow-argument-mismatch"
 
 # serial version
-make PROGRAM=RuNNer.serial.x
+make DEBUG=OFF MKL=OFF PROGRAM=RuNNer.serial.x
 cp RuNNer.serial.x ${PREFIX}/bin
 make clean
 
 # mpi parallel version
-make PROGRAM=RuNNer.mpi.x
+make DEBUG=OFF MKL=OFF PROGRAM=RuNNer.mpi.x
 cp RuNNer.mpi.x ${PREFIX}/bin
